71e3e8a7b15193c8fd00647f67441fe76ee78ff8
Sy <syht1602@gmail.com>
2023-06-07 12:30:39 +0700

    Add base adapter and base view holder

--- andfun-kotlin-sleep-tracker-starter-code/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
+++ andfun-kotlin-sleep-tracker-starter-code/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -1,41 +1,14 @@
 package com.example.android.trackmysleepquality.sleeptracker
 
-<<<<<<< Updated upstream
-import android.graphics.Color
-import android.view.LayoutInflater
-=======
 import android.view.View
->>>>>>> Stashed changes
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import com.example.android.trackmysleepquality.R
+import com.example.android.trackmysleepquality.convertDurationToFormatted
+import com.example.android.trackmysleepquality.convertNumericQualityToString
 import com.example.android.trackmysleepquality.database.SleepNight
 
-<<<<<<< Updated upstream
-class SleepNightAdapter : RecyclerView.Adapter<TextItemViewHolder>() {
-    var data = listOf<SleepNight>()
-        set(value) {
-            field = value
-            notifyDataSetChanged()
-        }
-
-    override fun getItemCount() = data.size
-
-    override fun onBindViewHolder(holder: TextItemViewHolder, position: Int) {
-        val night = data[position]
-        holder.textView.text = night.sleepQuality.toString()
-        if (night.sleepQuality <= 1) {
-            holder.textView.setTextColor(Color.RED)
-        } else {
-            holder.textView.setTextColor(Color.BLACK)
-        }
-    }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TextItemViewHolder {
-        val layoutInflater = LayoutInflater.from(parent.context)
-        val view = layoutInflater.inflate(R.layout.text_item_view, parent, false) as TextView
-        return TextItemViewHolder(view)
-=======
 class SleepNightAdapter : BaseAdapter<SleepNight, SleepNightAdapter.SleepNightViewHolder>() {
     override fun setLayoutId() = R.layout.list_item_sleep_night
 
@@ -68,12 +41,5 @@ class SleepNightAdapter : BaseAdapter<SleepNight, SleepNightAdapter.SleepNightVi
                 }
             )
         }
->>>>>>> Stashed changes
     }
 }
-<<<<<<< Updated upstream
-
-class TextItemViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
-
-=======
->>>>>>> Stashed changes
